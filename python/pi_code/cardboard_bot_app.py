027b6def7b615f7ae77cdf4cc9e877f174c78315
luno <lunosnyper@gmail.com>
2017-02-13 17:55:14 -0600

    working on adding firmata functionallity

--- python/pi_code/cardboard_bot_app.py
+++ python/pi_code/cardboard_bot_app.py
@@ -1,21 +1,65 @@
 import falcon
 import json
+from pyfirmata import Arduino, util
 
-l_motor = 0
-r_motor = 0
+l_motor_speed = 0
+r_motor_speed = 0
+
+l_motor
+l_dir1 = 2
+l_dir2 = 3
+
+r_motor
+r_dir1 = 4
+r_dir2 = 5
+
+board
+
+left_dir1_pin
+left_dir2_pin
+left_pwm
+
+right_dir1_pin
+right_dir2_pin
+right_pwm
 
 def get_status():
-    return 'POST -> Left Motor: ' + str(l_motor) + ', Right Motor: ' + str(r_motor)
+    return 'POST -> Left Motor: ' + str(l_motor_speed) + ', Right Motor: ' + str(r_motor_speed)
 
 def set_l_motor(speed):
-    global l_motor
+    global l_motor_speed
     print 'Setting Left Motor to: ' + str(speed)
-    l_motor = speed
+    l_motor_speed = speed
     
 def set_r_motor(speed):
-    global r_motor
+    global r_motor_speed
     print 'Setting Right Motor to: ' + str(speed)
-    r_motor = speed
+    r_motor_speed = speed
+
+def setup(device_path):
+    global board
+    global left_dir1_pin
+    global left_dir2_pin
+    global left_pwm
+    global right_dir1_pin
+    global right_dir2_pin
+    global right_pwm
+    global l_motor
+    global l_dir1
+    global l_dir2
+    global r_motor
+    global r_dir1
+    global r_dir2
+
+    board = Arduino(device_path)
+
+    left_dir1_pin = board.get_pin('a:' + l_dir1 + ':i')
+    left_dir2_pin = board.get_pin('a:' + l_dir2 + ':i')
+    left_pwm = board.get_pin('d:' + l_motor + ':p')
+
+    right_dir1_pin = board.get_pin('a:' + r_dir1 + ':i')
+    right_dir2_pin = board.get_pin('a:' + r_dir2 + ':i')
+    right_pwm = board.get_pin('d:' + r_motor + ':p')
 
 class CardBoardBotResource(object):
     
